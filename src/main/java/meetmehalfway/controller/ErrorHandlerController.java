575e794b86c3decc9f0a096ed1a755b112d5ac0e
MiguelM Duarte <miguelm.duarte@blip.pt>
2020-06-11 15:52:18 +0100

    add details to error page

--- src/main/java/meetmehalfway/controller/ErrorHandlerController.java
+++ src/main/java/meetmehalfway/controller/ErrorHandlerController.java
@@ -5,13 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class ErrorHandlerController implements ErrorController{
 
-    @Override
     @RequestMapping("/error")
     @ResponseBody
+    public String handleError(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        return String.format("<html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
+                        + "<div>Exception Message: <b>%s</b></div><body></html>",
+                statusCode, exception==null? "N/A": exception.getMessage());
+    }
+
+    @Override
     public String getErrorPath() {
-        return "<center><h1>Something went wrong</h1></center>";
+        return "/error";
     }
 }
