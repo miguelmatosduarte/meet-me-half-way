890a7ed2f2ad0a8bf7388990a41c7f90be659980
MiguelM Duarte <miguelm.duarte@blip.pt>
2020-06-11 15:32:13 +0100

    remove componentscan annotation

--- src/main/java/meetmehalfway/Application.java
+++ src/main/java/meetmehalfway/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"meetmehalfway.controller", "meetmehalfway.model", "meetmehalfway.utils"})
 public class Application {
 
 	public static void main(String[] args) {
